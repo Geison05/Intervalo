e1c64716f335bf47cfd9a7febd285825665843a5
Geison Amorim <geisonam43@gmail.com>
2022-04-12 10:23:48 -0300

    Add project files.

---
+++ Intervalo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variavel 
+            double N;
+
+            //Entrada
+            N = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Processamento / Saida
+            if (N < 0.0 || N > 100.0)
+                Console.WriteLine("Fora de Intervalo");
+            else if (N <= 25.0)
+                Console.WriteLine("Intervalo (0,25)");
+            else if(N <= 50.0)
+                Console.WriteLine("Intervalo (25,50)");
+            else if(N <= 75.0)
+                Console.WriteLine("Intervalo (50,75)");
+            else
+                Console.WriteLine("intervalo (75,100)");
+        }
+    }
+}
